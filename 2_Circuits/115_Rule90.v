f6a9a7bec128ce8c11f71e57f6c3800c2c3e6ce3
how861025ard <75475482+how861025ard@users.noreply.github.com>
2020-12-07 10:25:34 +0800

    Create 115_Rule90.v

---
+++ 2_Circuits/115_Rule90.v
@@ -0,0 +1,23 @@
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q ); 
+    
+    integer i;
+    
+    always@(posedge clk)begin
+        if(load)
+            q <= data;
+        else begin
+            for(i = 0;i < 512; i++)begin
+                if(i == 0) 
+                    q[0] <= q[1] ^ 0;
+                else if(i == 511)
+                    q[511] <= q[510] ^ 0;
+                else	
+                    q[i] <= q[i-1] ^ q[i+1];
+            end
+        end   
+    end
+endmodule
