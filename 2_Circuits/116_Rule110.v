2a6fb89ac677decb727297b34eb77f455b71671c
how861025ard <75475482+how861025ard@users.noreply.github.com>
2020-12-15 20:45:28 +0800

    Update 116_Rule110.v

--- 2_Circuits/116_Rule110.v
+++ 2_Circuits/116_Rule110.v
@@ -4,20 +4,11 @@ module top_module(
     input [511:0] data,
     output [511:0] q
 ); 
-    integer i;
-    
     always@(posedge clk)begin
         if(load)
             q <= data;
         else begin
-            for(i = 0;i < 512; i++)begin
-                if(i == 0) 
-                    q[0] <= ~q[0]&0 + q[0]&~0 + ~q[1]&0;
-                else if(i == 511)
-                    q[511] <= ~q[511]&q[510] + q[511]&~q[510] + ~0&q[510];
-                else	
-                    q[i] <= ~q[i]&q[i-1] + q[i]&~q[i-1] + ~q[i+1]&q[i-1];
-            end
-        end   
+            q[511:0] <= (q[511:0] ^ {q[510:0], 1'b0}) | ((~{1'b0, q[511:1]}) & q[511:0]); 
+        end
     end
 endmodule
