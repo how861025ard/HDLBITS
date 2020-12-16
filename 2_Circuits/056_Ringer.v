296a0ebd85c4c9da022e079729e421f3b29b5e99
how861025ard <75475482+how861025ard@users.noreply.github.com>
2020-12-16 16:26:46 +0800

    Create 056_Ringer.v

---
+++ 2_Circuits/056_Ringer.v
@@ -0,0 +1,11 @@
+module top_module (
+    input ring,
+    input vibrate_mode,
+    output ringer,       // Make sound
+    output motor         // Vibrate
+);
+
+    assign ringer = ring & ~vibrate_mode? 1 : 0;
+    assign motor = ring & vibrate_mode ?1 :0;
+    
+endmodule
