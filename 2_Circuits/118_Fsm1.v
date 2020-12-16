49b5d503ad78adce39848519861cb7f29719ca85
how861025ard <75475482+how861025ard@users.noreply.github.com>
2020-12-16 13:27:13 +0800

    Create 118_Fsm1.v

---
+++ 2_Circuits/118_Fsm1.v
@@ -0,0 +1,27 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to state B
+    input in,
+    output out);//  
+
+    parameter A=0, B=1; 
+    reg state, next_state;
+
+    always @(*) begin    // This is a combinational always block
+        case (state)// State transition logic
+            A: next_state <= in ? A : B;
+            B: next_state <= in ? B : A;
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin    // This is a sequential always block
+        if(areset)// State flip-flops with asynchronous reset
+            state <= B;
+        else
+            state <= next_state;
+    end
+
+    // Output logic
+    assign out = (state == B);
+
+endmodule
