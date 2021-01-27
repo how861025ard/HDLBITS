fc7267a89cb3c08e56800e5b04c7b0bdb01a1fda
how861025ard <75475482+how861025ard@users.noreply.github.com>
2021-01-27 21:48:50 +0800

    Create 152_Exams_review2015_count1k.v

---
+++ 2_Circuits/152_Exams_review2015_count1k.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input reset,
+    output [9:0] q);
+	
+    always@(posedge clk)begin
+        if(reset)
+            q <= 0;
+        else if(q == 999)
+            q <= 0;
+        else
+            q <= q + 1;
+    end
+    
+endmodule
