6dc9ea78028fc2a610bb90f22b691ee5a0d9cded
how861025ard <75475482+how861025ard@users.noreply.github.com>
2021-01-31 10:59:10 +0800

    Create 172_Sim_circuit9.v

---
+++ 3_Verification_Reading_Simulations/172_Sim_circuit9.v
@@ -0,0 +1,10 @@
+module top_module (
+    input clk,
+    input a,
+    output [3:0] q );
+	
+    always@(posedge clk)begin
+        if(a == 1) q <= 4;
+        else q <= (q + 1) % 7;
+    end
+endmodule
